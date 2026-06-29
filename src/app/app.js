b8d7fc67c7e02bfac47c2825445a681b4be2e938
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:50:09 +0700

    Create app.js

---
+++ src/app/app.js
@@ -0,0 +1,41 @@
+export function renderApp(){
+
+    document.querySelector(
+
+        "#app"
+
+    ).innerHTML =
+
+`
+<div class="app">
+
+<header>
+
+<h1>
+
+LaunchFuture
+
+</h1>
+
+<p>
+
+Professional Multichain Launch Platform
+
+</p>
+
+</header>
+
+<main>
+
+<button id="connect">
+
+Connect Wallet
+
+</button>
+
+</main>
+
+</div>
+`;
+
+}
