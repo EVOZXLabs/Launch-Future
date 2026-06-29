4a43284baad01205803f96118e3b7bf51c9f0a8a
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:17:37 +0700

    Create network.js

---
+++ src/config/network.js
@@ -0,0 +1,79 @@
+export const NETWORKS = {
+
+    evoz: {
+
+        id: "evoz",
+
+        name: "EVOZ Mainnet",
+
+        chainId: 20120,
+
+        symbol: "EVOZ",
+
+        rpc: "",
+
+        explorer: "",
+
+        factory: ""
+
+    },
+
+    ethereum: {
+
+        id: "ethereum",
+
+        name: "Ethereum",
+
+        chainId: 1,
+
+        symbol: "ETH",
+
+        rpc: "",
+
+        explorer: "",
+
+        factory: ""
+
+    },
+
+    bsc: {
+
+        id: "bsc",
+
+        name: "BNB Smart Chain",
+
+        chainId: 56,
+
+        symbol: "BNB",
+
+        rpc: "",
+
+        explorer: "",
+
+        factory: ""
+
+    },
+
+    polygon: {
+
+        id: "polygon",
+
+        name: "Polygon",
+
+        chainId: 137,
+
+        symbol: "POL",
+
+        rpc: "",
+
+        explorer: "",
+
+        factory: ""
+
+    }
+
+};
+
+export const DEFAULT_NETWORK =
+
+    "evoz";
