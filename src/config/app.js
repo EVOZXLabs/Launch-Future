97d06ce3d53752a533ec2513a3fbed6bae6b3684
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:26:46 +0700

    Create app.js

---
+++ src/config/app.js
@@ -0,0 +1,27 @@
+export const APP = {
+
+    NAME:
+
+        "LaunchFuture",
+
+    VERSION:
+
+        "2.0.0",
+
+    AUTHOR:
+
+        "EVOZXLabs",
+
+    DEFAULT_THEME:
+
+        "dark",
+
+    DEFAULT_NETWORK:
+
+        "evoz",
+
+    STORAGE_PREFIX:
+
+        "launchfuture"
+
+};
