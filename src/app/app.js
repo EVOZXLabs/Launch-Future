03f67ab1176b4651fb5132fe06c1e46e19fadef5
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:59:16 +0700

    Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,24 @@
-export function renderApp() {
+import {
+
+    Topbar
+
+}
+
+from
+
+"../components/topbar.js";
+
+import {
+
+    Workspace
+
+}
+
+from
+
+"../components/workspace.js";
+
+export function renderApp(){
 
     document.querySelector(
 
@@ -7,85 +27,15 @@ export function renderApp() {
     ).innerHTML =
 
 `
+
 <div id="layout">
 
-<header id="topbar">
+${Topbar()}
 
-<div class="brand">
-
-<h1>
-
-LaunchFuture
-
-</h1>
-
-<p>
-
-Professional Multichain Launch Platform
-
-</p>
+${Workspace()}
 
 </div>
 
-<div class="topbarActions">
-
-<button
-
-id="networkButton">
-
-EVOZ Mainnet
-
-</button>
-
-<button
-
-id="connectWalletButton">
-
-Connect Wallet
-
-</button>
-
-</div>
-
-</header>
-
-<main id="workspace">
-
-<section id="wizardPanel">
-
-<h2>
-
-Welcome
-
-</h2>
-
-<p>
-
-Select a network and connect your wallet to begin creating your token.
-
-</p>
-
-</section>
-
-<aside id="previewPanel">
-
-<h3>
-
-Live Preview
-
-</h3>
-
-<p>
-
-Waiting for project information...
-
-</p>
-
-</aside>
-
-</main>
-
-</div>
 `;
 
 }
