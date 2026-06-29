7fbf30fe8668b4d5ced957fa2954aee57d0b8ae7
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:31:25 +0700

    Create dom.js

---
+++ src/utils/dom.js
@@ -0,0 +1,27 @@
+export const $ = (
+
+    id
+
+)=>
+
+document.getElementById(
+
+    id
+
+);
+
+export const $$ = (
+
+    selector
+
+)=>
+
+[
+
+    ...document.querySelectorAll(
+
+        selector
+
+    )
+
+];
