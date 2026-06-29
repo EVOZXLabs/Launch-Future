5c8a34fb2ce6d11cb40c248d8c8ca752c3fdabbd
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-29 19:57:42 +0700

    Create topbar.js

---
+++ src/components/topbar.js
@@ -0,0 +1,45 @@
+export function Topbar() {
+
+    return `
+
+<header id="topbar">
+
+<div class="brand">
+
+<h1>
+
+LaunchFuture
+
+</h1>
+
+<p>
+
+Professional Multichain Launch Platform
+
+</p>
+
+</div>
+
+<div class="topbarActions">
+
+<button
+id="networkButton">
+
+EVOZ Mainnet
+
+</button>
+
+<button
+id="connectWalletButton">
+
+Connect Wallet
+
+</button>
+
+</div>
+
+</header>
+
+`;
+
+}
